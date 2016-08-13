a78f8855b453b6987688167155a615090409fde4
Konrad Winkowski <winkowskikonrad@gmail.com>
2016-08-13 13:08:37 -0400

    odd/even
    fixed code to handle odd or even spiral loop

--- planet_generation_test/GameScene.swift
+++ planet_generation_test/GameScene.swift
@@ -14,7 +14,7 @@ class GameScene: SKScene {
     
     override func didMoveToView(view: SKView) {
         
-        planet_info = [[Int]](count:10, repeatedValue:[Int](count:10, repeatedValue:0))
+        planet_info = [[Int]](count:4, repeatedValue:[Int](count:4, repeatedValue:0))
         
         for index_y in 0..<planet_info!.count {
             for index_x in 0..<planet_info!.count {
@@ -28,6 +28,11 @@ class GameScene: SKScene {
             }
         }
         
+        planet_info = [[15,4,5,6],
+                       [14,3,0,7],
+                       [13,2,1,8],
+                       [12,11,10,9]]
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -36,7 +41,7 @@ class GameScene: SKScene {
         
         
         let center_x = planet_info!.count / 2;
-        let center_y = center_x
+        let center_y = planet_info!.count % 2 == 0 ? center_x - 1 : center_x
         
         var irritator_x = center_x
         var irritator_y = center_y + 1
@@ -46,41 +51,60 @@ class GameScene: SKScene {
         var move_up = 2
         var move_right = 2
         
-        print(planet_info![center_x][center_y])
+        print(planet_info![center_y][center_x])
         
-        while irritator_x < planet_info!.count && irritator_y < planet_info!.count {
+        var loop = true
+        
+        while loop && irritator_x < planet_info!.count && irritator_y < planet_info!.count {
             
             // left //
             let end_left = irritator_x - move_left
+            var index_left = irritator_x
             
-            for var index_left = irritator_x; index_left > end_left; index_left -= 1 {
+            while index_left > end_left && irritator_x > 0 {
                 print(planet_info![irritator_y][index_left])
                 
+                index_left -= 1
                 irritator_x -= 1
             }
             
             // up //
             let end_up = irritator_y - move_up
+            var index_up = irritator_y
             
-            for var index_up = irritator_y; index_up > end_up; index_up -= 1 {
+            while index_up > end_up && index_up > 0 {
                 print(planet_info![index_up][irritator_x])
+                
+                index_up -= 1
                 irritator_y -= 1
+            }
+            
+            if end_up < 0 {
+                print(planet_info![irritator_y][irritator_x])
+                loop = false
+                continue
             }
             
             // right //
             let end_right = irritator_x + move_right
+            var index_right = irritator_x
             
-            for var index_right = irritator_x; index_right < end_right; index_right += 1 {
+            while index_right < end_right && index_right < planet_info!.count - 1 {
                 print(planet_info![irritator_y][index_right])
+                
+                index_right += 1
                 irritator_x += 1
             }
             
             // down //
             move_down += 2
             let end_down = irritator_y + move_down
+            var index_down = irritator_y
             
-            for var index_down = irritator_y; index_down < end_down; index_down += 1 {
+            while index_down < end_down && index_down < planet_info!.count - 1 {
                 print(planet_info![index_down][irritator_x])
+                
+                index_down += 1
                 irritator_y += 1
             }
             

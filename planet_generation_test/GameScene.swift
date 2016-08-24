c56c05dc2b76982356430acc80284f501f7bf891
Konrad Winkowski <winkowskikonrad@gmail.com>
2016-08-24 16:03:38 -0400

    creates planet
    very in efficient

--- planet_generation_test/GameScene.swift
+++ planet_generation_test/GameScene.swift
@@ -11,35 +11,100 @@ import SpriteKit
 class GameScene: SKScene {
     
     var planet_info : [[Int]]?
+    var planet_center_point : CGPoint?
+    var player : SKSpriteNode?
+    var cam : SKCameraNode!
     
     override func didMoveToView(view: SKView) {
         
-        planet_info = [[Int]](count:4, repeatedValue:[Int](count:4, repeatedValue:0))
+        view.ignoresSiblingOrder = true
+        
+        planet_info = [[Int]](count:500, repeatedValue:[Int](count:500, repeatedValue:0))
+        
+        let center_val = CGFloat((planet_info?.count)! / 2)
+        
+        planet_center_point = CGPointMake(center_val,center_val)
+        
+        generatePlanet()
         
         for index_y in 0..<planet_info!.count {
             for index_x in 0..<planet_info!.count {
-                planet_info![index_y][index_x] = Int(arc4random_uniform(100))
+                let val = planet_info![index_y][index_x]
                 
-                let node = SKSpriteNode.init(color: UIColor.redColor(), size: CGSizeMake(10, 10))
-                node.name = "\(index_y)\(index_x)"
-                node.position = CGPointMake(CGFloat(index_x) * 10, CGFloat(index_y) * 10)
-                self.addChild(node)
+                if val == 1 {
+                    let node = SKSpriteNode.init(color: UIColor.redColor(), size: CGSizeMake(10, 10))
+                    node.name = "\(index_y)\(index_x)"
+                    node.position = CGPointMake(CGFloat(index_x) * 10, CGFloat(index_y) * 10)
+                    node.zPosition = 1;
+                    self.addChild(node)
+                }
                 
             }
         }
         
-        planet_info = [[15,4,5,6],
-                       [14,3,0,7],
-                       [13,2,1,8],
-                       [12,11,10,9]]
+        player = SKSpriteNode(color: UIColor.greenColor(), size: CGSizeMake(40, 40))
+        player?.name = "player"
+        self.addChild(player!)
+        
+        cam = SKCameraNode() //initialize and assign an instance of SKCameraNode to the cam variable.
+        
+        self.camera = cam //set the scene's camera to reference cam
+        self.addChild(cam) //make the cam a childElement of the scene itself.
+        
+        //position the camera on the gamescene.
+        cam.position = (player?.position)!
+        
+        //
+        //        planet_info = [[15,4,5,6],
+        //                       [14,3,0,7],
+        //                       [13,2,1,8],
+        //                       [12,11,10,9]]
         
     }
     
+    func generatePlanet() {
+        for index_y in 0..<planet_info!.count {
+            for index_x in 0..<planet_info!.count {
+                let index_point = CGPointMake(CGFloat(index_x), CGFloat(index_y))
+                
+                let distance = distanceBetweenPoints(planet_center_point!, toPoint: index_point)
+                
+                if distance < 250 {
+                    planet_info![index_y][index_x] = 1
+                }
+                
+                print(distance)
+            }
+        }
+    }
+    
+    func distanceBetweenPoints(center : CGPoint, toPoint : CGPoint) -> CGFloat {
+        let xDist = (toPoint.x - center.x);
+        let yDist = (toPoint.y - center.y);
+        let distance = sqrt((xDist * xDist) + (yDist * yDist));
+        
+        return fabs(distance)
+    }
+    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
-        
-        
-        
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            let previousLocation = touch.previousLocationInNode(self)
+            let deltaY = location.y - previousLocation.y
+            let deltaX = location.x - previousLocation.x
+            cam.position.y += deltaY
+            cam.position.x += deltaX
+        }
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+    
+    func spiralOut() {
         let center_x = planet_info!.count / 2;
         let center_y = planet_info!.count % 2 == 0 ? center_x - 1 : center_x
         
@@ -119,10 +184,5 @@ class GameScene: SKScene {
             move_up += 2
             move_right += 2
         }
-        
-    }
-    
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
     }
 }
